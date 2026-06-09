59dd3a7b0bb715f59b287451c818680ad7c0c7de
Hitesh Gehlot <mrhiteshgehlot@gmail.com>
2026-06-09 21:42:19 +0530

    added CPU-MEMORY-DISK Info

--- monitor.sh
+++ monitor.sh
@@ -10,10 +10,37 @@ echo "Uptime: "
 uptime
 
 
+############ CPU Information ##############
+
+echo
 echo "CPU Information: "
 
 lscpu | grep "Model name\|CPU MHz\|CPU(s)"
 
+echo
 echo "CPU Usage: "
 top -bn1 | grep "Cpu(s)"
 
+########## Memory Usage ###############
+
+echo
+echo "Memory Usage:"
+free -h
+
+########## Disk Usage ###############
+echo
+echo "Disk Usage: "
+df -h
+
+
+########## Log Information ##############
+
+echo
+echo "Recent System Logs: "
+
+LOGFILE="logs/health.log"
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+
+echo "[$DATE] System health check performed." >> $LOGFILE
+
+cat logs/health.log
